ce0236c0ec126a508a3f634a22fa8c9caa9b2e9b
Christopher Brown <ccbrown112@gmail.com>
2021-01-06 13:31:04 -0500

    Initial Commit

---
+++ CognitoAuthSample/CognitoAuthSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CognitoAuthSampleApp.swift
+//  CognitoAuthSample
+//
+//  Created by Christopher Brown on 1/6/21.
+//
+
+import SwiftUI
+
+@main
+struct CognitoAuthSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
